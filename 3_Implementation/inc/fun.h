039e70de71257600e15fcb9caf554fb2e3452c29
Chinnam-Narendra-Prasad <82199636+Chinnam-Narendra-Prasad@users.noreply.github.com>
2021-04-15 14:44:09 +0530

    Add files via upload

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+void login ();
+void mainMenu ();
+float checkBalance (float balance);
+float moneyDeposit (float balance);
+float moneyWithdraw (float balance);
+void menuExit ();
+void errorMessage ();
